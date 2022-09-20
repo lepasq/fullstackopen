f6607a7292b38840050eeb8d547b821c42a0c7ad
lepasq <pasquarelli.le@gmail.com>
2022-09-20 19:21:52 +0300

    part4: <= 4.12

---
+++ part4/blog/tests/blog_api.test.js
@@ -0,0 +1,129 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+
+beforeEach(async () => {
+	await Blog.deleteMany({})
+	let blogObject = new Blog(initialBlogs[0])
+	await blogObject.save()
+	blogObject = new Blog(initialBlogs[1])
+	await blogObject.save()
+
+	// for(let blog of initialBlogs) {
+	//   let blogObj = new Blog(blog)
+	//   await blogObj.save()
+	// }
+})
+
+
+const initialBlogs = [
+	{
+		'title': "Blog post",
+		'author': "Leonardo Pasquarelli",
+		'url': "http://google.com",
+		'likes': 3
+	},
+	{
+		'title': "Second post post",
+		'author': "Tuomas Pierra",
+		'url': "http://youtube.com",
+		'likes': 5
+	}
+]
+
+
+test('blogs are returned as json', async () => {
+	const response = await api.get('/api/blogs');
+	expect(response.body).toHaveLength(initialBlogs.length);
+}, 100000)
+
+
+test('blogs contain unique id', async () => {
+	const response = await api.get('/api/blogs');
+	response.body.forEach(blog => {
+		expect(blog.id).toBeDefined();
+		expect(blog._id).toBeUndefined();
+	})
+}, 300000)
+
+
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+		'title': "Another post",
+		'author': "Leonardo Pasquarelli",
+		'url': "http://reddit.com",
+		'likes': 9
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const url = response.body.map(r => r.url)
+
+  expect(response.body).toHaveLength(initialBlogs.length + 1)
+	expect(url).toContainEqual(
+		'http://reddit.com'
+	)
+})
+
+
+test('a blog without likes defaults to 0', async () => {
+  const newBlog = {
+		'title': "Hello there",
+		'author': "Leonardo Pasquarelli",
+		'url': "http://amazon.com"
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const blog = response.body.find(x => x.title === 'Hello there')
+
+  expect(response.body).toHaveLength(initialBlogs.length + 1)
+	expect(blog).toBeDefined()
+	expect(blog.likes).toEqual(0)
+})
+
+
+test('blog without title or url is not added', async () => {
+	const newBlogs = [{
+		'author': "Leonardo Pasquarelli",
+		'url': "http://facebook.com",
+		'likes': 0
+		},
+		{
+			'title': "Book of faces",
+			'author': "Leonardo Pasquarelli",
+			'likes': 0
+		}]
+
+	for (let newBlog of newBlogs) {
+		await api
+			.post('/api/blogs')
+			.send(newBlog)
+			.expect(400)
+
+		const response = await api.get('/api/blogs')
+		expect(response.body).toHaveLength(initialBlogs.length)
+	}
+})
+
+
+afterAll(() => {
+  mongoose.connection.close()
+})
