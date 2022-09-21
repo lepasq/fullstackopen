81131e94a7ac641e42f07d08c9d27334f3748b3a
lepasq <pasquarelli.le@gmail.com>
2022-09-21 14:56:13 +0300

    part4: changes part 4.3

--- part4/blog/controllers/blog.js
+++ part4/blog/controllers/blog.js
@@ -1,15 +1,20 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog
+		.find({}).populate('user', {username: 1, name: 1})
 	response.json(blogs)
 })
 
 blogRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const user = await User.findById(body.userId)
+  const blog = new Blog(request.body.push({user: user._id}))
   const result = await blog.save()
 	if(result) {
+		user.blogs = user.blogs.concat(result._id)  
+		await user.save()
 		response.status(201).json(result)
 	} else {
 		response.status(400).end()
